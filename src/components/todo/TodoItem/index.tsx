63edd3e58c90792f934c0ce86bd7df684a5d561b
streakingman <z_max_y@163.com>
2022-04-13 04:39:53 +0800

    chore: todoItem animate test

--- src/components/todo/TodoItem/index.tsx
+++ src/components/todo/TodoItem/index.tsx
@@ -1,12 +1,50 @@
-import React, { FC } from 'react';
+import React, { FC, ReactEventHandler, useState } from 'react';
 import classnames from 'classnames';
 import style from './index.module.scss';
 import { TodoItemProps } from './interface';
 import { Close } from '@mui/icons-material';
 
+const initMatrix = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+];
+
 const TodoItem: FC<TodoItemProps> = ({ title, id, onDelete }) => {
+    const [matrix, setMatrix] = useState(initMatrix);
+    const [hasTransition, setHasTransition] = useState(true);
+    const handleMouseMove: ReactEventHandler = (e) => {
+        const { offsetX, offsetY, target } = e.nativeEvent as MouseEvent;
+        const x = offsetX - (target as HTMLElement).offsetWidth / 2;
+        const y = offsetY - (target as HTMLElement).offsetHeight / 2;
+        setMatrix([
+            [1, 0, 0, x * 0.0000005],
+            [0, 1, 0, y * 0.00001],
+            [0, 0, 1, 1],
+            [0, 0, 0, 1],
+        ]);
+        if (hasTransition) {
+            setTimeout(() => {
+                setHasTransition(false);
+            }, 300);
+        }
+    };
+    const handleMouseLeave = () => {
+        setHasTransition(true);
+        setMatrix(initMatrix);
+    };
+
     return (
-        <div className={classnames(style.todoItem)}>
+        <div
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+            className={classnames(style.todoItem)}
+            style={{
+                transform: `matrix3d(${matrix.toString()})`,
+                transition: hasTransition ? '0.3s' : undefined,
+            }}
+        >
             {title}
             <Close onClick={() => onDelete(id)} />
         </div>
